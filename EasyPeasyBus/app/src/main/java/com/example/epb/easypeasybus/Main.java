5a6b2680f82ceb0ea4afc923a4bad4f2df95c5e3
Juanjo <juanjozv@gmail.com>
2018-04-13 01:34:04 -0600

    Initial maps module

--- EasyPeasyBus/app/src/main/java/com/example/epb/easypeasybus/Main.java
+++ EasyPeasyBus/app/src/main/java/com/example/epb/easypeasybus/Main.java
@@ -1,7 +1,11 @@
 package com.example.epb.easypeasybus;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.view.View;
 
 public class Main extends AppCompatActivity {
 
@@ -9,5 +13,47 @@ public class Main extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //CardViews
+        OnclickDelCardView(R.id.cardViewMapa);
+        OnclickDelCardView(R.id.cardViewPlanificador);
+        OnclickDelCardView(R.id.cardViewGastos);
+        OnclickDelCardView(R.id.cardViewHorarios);
+    }
+
+    public void OnclickDelCardView(int ref) {
+        View view =findViewById(ref);
+        CardView miCardView = (CardView) view;
+        miCardView.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                switch (v.getId()) {
+                    case R.id.cardViewMapa:
+                        Intent mapIntent = new Intent(getApplicationContext(), Maps.class);
+                        startActivity(mapIntent);
+                        break;
+
+                    case R.id.cardViewPlanificador:
+                        String url = "http://maps.google.com/maps?saddr="+9.996557+","+-84.085443+"&daddr="+9.984507+","+-84.086754+"&mode=transit";
+                        //"http://maps.google.com/maps?saddr="+ starting_point_lat+","+starting_point_long+"&daddr="+dest_point_lat+","+dest_point_long+"&mode=transit"
+
+                        Intent intent = new Intent(android.content.Intent.ACTION_VIEW,
+                                Uri.parse(url));
+                        startActivity(intent);
+
+                        break;
+
+                    case R.id.cardViewGastos:
+
+
+                        break;
+
+                    case R.id.cardViewHorarios:
+
+                        break;
+                    default:break;
+                }
+            }
+        });
     }
 }
---
+++ EasyPeasyBus/app/src/main/java/com/example/epb/easypeasybus/Prueba.java
@@ -0,0 +1,65 @@
+package com.example.epb.easypeasybus;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
+import com.google.maps.DirectionsApi;
+import com.google.maps.GeoApiContext;
+import com.google.maps.android.PolyUtil;
+import com.google.maps.errors.ApiException;
+import com.google.maps.model.DirectionsResult;
+import com.google.maps.model.TravelMode;
+
+import org.joda.time.DateTime;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+public class Prueba extends AppCompatActivity {
+
+    public Prueba() throws InterruptedException, ApiException, IOException {
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_prueba);
+    }
+
+    private GeoApiContext getGeoContext() {
+        GeoApiContext geoApiContext = new GeoApiContext();
+        return geoApiContext.setQueryRateLimit(3)
+                .setApiKey(getString(R.string.google_maps_key))
+                .setConnectTimeout(1, TimeUnit.SECONDS)
+                .setReadTimeout(1, TimeUnit.SECONDS)
+                .setWriteTimeout(1, TimeUnit.SECONDS);
+    }
+
+    DateTime now = new DateTime();
+    LatLng actualLocation = new LatLng(9.988597,-84.097419);
+    DirectionsResult result = DirectionsApi.newRequest(getGeoContext())
+            .mode(TravelMode.DRIVING)
+            .origin("9.984507, -84.086754")
+            .destination("9.984507,-84.086754")
+            .departureTime(now)
+            .await();
+
+    private void addMarkersToMap(DirectionsResult results, GoogleMap mMap) {
+        mMap.addMarker(new MarkerOptions().position(new LatLng(results.routes[0].legs[0].startLocation.lat,results.routes[0].legs[0].startLocation.lng)).title(results.routes[0].legs[0].startAddress));
+        mMap.addMarker(new MarkerOptions().position(new LatLng(results.routes[0].legs[0].endLocation.lat,results.routes[0].legs[0].endLocation.lng)).title(results.routes[0].legs[0].startAddress).snippet(getEndLocationTitle(results)));
+    }
+
+    private String getEndLocationTitle(DirectionsResult results){
+        return  "Time :"+ results.routes[0].legs[0].duration.humanReadable + " Distance :" + results.routes[0].legs[0].distance.humanReadable;
+    }
+
+    private void addPolyline(DirectionsResult results, GoogleMap mMap) {
+        List<LatLng> decodedPath = PolyUtil.decode(results.routes[0].overviewPolyline.getEncodedPath());
+        mMap.addPolyline(new PolylineOptions().addAll(decodedPath));
+    }
+}
