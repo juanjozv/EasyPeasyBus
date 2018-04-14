2a777827c8b98d975f8f0f2ed249071e023a449b
Oscar <osqui.salazar@gmail.com>
2018-04-13 22:47:00 -0600

    Direction into two places is fixed

--- EasyPeasyBus/app/src/main/java/com/example/toshiba/easypeasybus/Maps.java
+++ EasyPeasyBus/app/src/main/java/com/example/toshiba/easypeasybus/Maps.java
@@ -1,8 +1,11 @@
 package com.example.toshiba.easypeasybus;
 
 import android.graphics.Color;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.akexorcist.googledirection.DirectionCallback;
 import com.akexorcist.googledirection.GoogleDirection;
@@ -10,25 +13,33 @@ import com.akexorcist.googledirection.constant.TransportMode;
 import com.akexorcist.googledirection.model.Direction;
 import com.akexorcist.googledirection.model.Leg;
 import com.akexorcist.googledirection.model.Route;
+import com.akexorcist.googledirection.model.Step;
 import com.akexorcist.googledirection.util.DirectionConverter;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.PolylineOptions;
 
 import java.util.ArrayList;
+import java.util.List;
 
-public class Maps extends FragmentActivity implements OnMapReadyCallback {
-
+public class Maps extends FragmentActivity implements OnMapReadyCallback, View.OnClickListener, DirectionCallback {
+    private Button btnRequestDirection;
     private GoogleMap mMap;
+    private String serverKey = "AIzaSyCzzfGJVG5IxORo5ZCeDLo3tReFUuS41Fw";
+    private LatLng origin = new LatLng(10.0159631, -84.21416699999999);
+    private LatLng destination = new LatLng(9.9277704, -84.0908422);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+        btnRequestDirection = findViewById(R.id.btn_request_direction);
+        btnRequestDirection.setOnClickListener(this);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
@@ -48,43 +59,63 @@ public class Maps extends FragmentActivity implements OnMapReadyCallback {
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
      */
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
+        this.mMap = googleMap;
+    }
 
-        // Add a marker in Sydney and move the camera
-        /*LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));*/
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        if (id == R.id.btn_request_direction) {
+            requestDirection();
+        }
+    }
 
-        String serverKey = "AIzaSyBL50qX0BwDn4C13Pv4-U_Z8YzzslFDTC4";
-        LatLng origin = new LatLng(9.9277704,  -84.0908422);
-        LatLng destination = new LatLng(10.0159631, -84.21416699999999);
+    public void requestDirection() {
+        Snackbar.make(btnRequestDirection, "Direction Requesting...", Snackbar.LENGTH_SHORT).show();
         GoogleDirection.withServerKey(serverKey)
                 .from(origin)
                 .to(destination)
-                .transportMode(TransportMode.DRIVING)
-                .alternativeRoute(true)
-                .execute(new DirectionCallback() {
-                    @Override
-                    public void onDirectionSuccess(Direction direction, String rawBody) {
-                        if(direction.isOK()) {
-                            Route route = direction.getRouteList().get(0);
-                            Leg leg = route.getLegList().get(0);
-                            ArrayList<LatLng> directionPositionList = leg.getDirectionPoint();
-                            PolylineOptions polylineOptions = DirectionConverter.createPolyline(getApplicationContext(), directionPositionList, 5, Color.RED);
-                            mMap.addPolyline(polylineOptions);
+                .transportMode(TransportMode.TRANSIT)
+                .execute(this);
+    }
 
-                        }
+    @Override
+    public void onDirectionSuccess(Direction direction, String rawBody) {
+        Snackbar.make(btnRequestDirection, "Success with status : " + direction.getStatus(), Snackbar.LENGTH_SHORT).show();
+        if (direction.isOK()) {
+            Route route = direction.getRouteList().get(0);
+            Leg leg = route.getLegList().get(0);
+            ArrayList<LatLng> sectionPositionList = leg.getSectionPoint();
+            for (LatLng position : sectionPositionList) {
+                mMap.addMarker(new MarkerOptions().position(position));
+            }
 
-                    }
+            List<Step> stepList = leg.getStepList();
+            ArrayList<PolylineOptions> polylineOptionList = DirectionConverter.createTransitPolyline(this, stepList, 5, Color.RED, 3, Color.BLUE);
+            for (PolylineOptions polylineOption : polylineOptionList) {
+                mMap.addPolyline(polylineOption);
+            }
+            setCameraWithCoordinationBounds(route);
 
-                    @Override
-                    public void onDirectionFailure(Throwable t) {
-                        // Do something here
-                    }
-                });
+            btnRequestDirection.setVisibility(View.GONE);
+        } else {
+            Snackbar.make(btnRequestDirection, direction.getStatus(), Snackbar.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void onDirectionFailure(Throwable t) {
+        Snackbar.make(btnRequestDirection, t.getMessage(), Snackbar.LENGTH_SHORT).show();
+    }
 
 
+    private void setCameraWithCoordinationBounds(Route route) {
+        LatLng southwest = route.getBound().getSouthwestCoordination().getCoordination();
+        LatLng northeast = route.getBound().getNortheastCoordination().getCoordination();
+        LatLngBounds bounds = new LatLngBounds(southwest, northeast);
+        mMap.animateCamera(CameraUpdateFactory.newLatLngBounds(bounds, 100));
     }
 }
