72e1cd33230f0d798c5ca06422313368c185ea99
Juanjo <juanjozv@gmail.com>
2018-04-13 20:23:47 -0600

    Maps activity

--- EasyPeasyBus/app/src/main/java/com/example/toshiba/easypeasybus/Main.java
+++ EasyPeasyBus/app/src/main/java/com/example/toshiba/easypeasybus/Main.java
@@ -1,8 +1,11 @@
 package com.example.toshiba.easypeasybus;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.widget.CardView;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -41,6 +44,12 @@ public class Main extends AppCompatActivity
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
+
+        //CardViews
+        OnclickDelCardView(R.id.cardViewMapa);
+        OnclickDelCardView(R.id.cardViewPlanificador);
+        OnclickDelCardView(R.id.cardViewGastos);
+        OnclickDelCardView(R.id.cardViewHorarios);
 
 
     }
@@ -100,5 +109,41 @@ public class Main extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    public void OnclickDelCardView(int ref) {
+        View view =findViewById(ref);
+        CardView miCardView = (CardView) view;
+        miCardView.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                switch (v.getId()) {
+                    case R.id.cardViewMapa:
+                        Intent intento = new Intent(getApplicationContext(), Maps.class);
+                        startActivity(intento);
+                        break;
+
+                    case R.id.cardViewPlanificador:
+                        String url = "http://maps.google.com/maps?saddr="+9.996557+","+-84.085443+"&daddr="+9.984507+","+-84.086754+"&mode=transit";
+                        //"http://maps.google.com/maps?saddr="+ starting_point_lat+","+starting_point_long+"&daddr="+dest_point_lat+","+dest_point_long+"&mode=transit"
+
+                        Intent intent = new Intent(android.content.Intent.ACTION_VIEW,
+                                Uri.parse(url));
+                        startActivity(intent);
+
+                        break;
+
+                    case R.id.cardViewGastos:
+
+
+                        break;
+
+                    case R.id.cardViewHorarios:
+
+                        break;
+                    default:break;
+                }
+            }
+        });
     }
 }
